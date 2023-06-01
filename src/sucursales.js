bd8b8bd63efc18fd79df7df85a4333d47c94a73f
MateoMariotti <mateomariotti77@gmail.com>
2023-05-31 23:31:20 -0600

    agregando mi parte

---
+++ src/sucursales.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let sucursales = {
+    db:'./data/theaters.json',
+    titulo: 'Nuestras Salas',
+    leerJSON: function(){
+        let theatersJSON = fs.readFileSync(this.db, 'utf-8');
+        let salas = JSON.parse(theatersJSON);
+        return salas
+    },
+    cantidad: function (){
+        return this.leerJSON().total_theaters
+    },
+    listarSalas: function (){
+        let theaters = this.leerJSON();
+        return theaters.theaters
+        }
+        
+    }
